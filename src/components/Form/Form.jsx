6b0b61950b336ae0d523cc440cb30f861b806aec
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-03-08 15:58:21 +0200

    add gallery

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,40 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-const Form = () => {
-  return <h2>Form</h2>;
+const Form = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = e => {
+    setQuery(e.target.value.trim());
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (query === '') return alert('Write your request');
+    onSubmit(query);
+
+    setQuery('');
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        value={query}
+        onChange={handleInputChange}
+      />
+    </form>
+  );
 };
 
 export default Form;
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from 'react';
+import { getPhotos } from '../apiService/photos';
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
+import Loader from '../components/Loader/Loader';
+import PhotosGallery from '../components/PhotosGallery/PhotosGallery';
+import Button from '../components/Button/Button';
 
 const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+
+  const getQuery = value => {
+    setQuery(value);
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (!query) return;
+        setLoading(true);
+
+        const { photos, per_page, total_results } = await getPhotos(
+          query,
+          page
+        );
+        if (!photos.length) {
+          setIsEmpty(true);
+          return;
+        }
+
+        setImages(prevImages => [...prevImages, ...photos]);
+        setLoadMore(page < Math.ceil(total_results / per_page));
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [query, page]);
+
+  const handleBtnClick = () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+  };
+
   return (
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={getQuery} />
+      {loading && <Loader />}
+      {error && <Text textAlign="center">Something went wrong!</Text>}
+      {images.length > 0 && <PhotosGallery images={images} />}
+      {isEmpty && <Text textAlign="center">Nothing found!</Text>}
+      {loadMore && <Button onClick={handleBtnClick}>Load More</Button>}
     </>
   );
 };
